c0056d0047e603ce2b5297cc7f885dc3fce5682f
zsdostar <zsdzzydos@gmail.com>
2017-06-08 13:57:43 +0800

    利用页面ID进行遍历下载页面

---
+++ BookWebScrapingWIthPython/Chapter1-Crawling/IDCrawling.py
@@ -0,0 +1,30 @@
+# coding:utf8
+# __author__ = 'zsdostar'
+# __date__ = '2017/6/8 13:48'
+import itertools
+from BasicWellCrawlingCode import download
+"""这段代码演示了如何利用ID的连续性进行遍历爬取页面"""
+
+# 以下代码并不健壮，因为如果出现了不连续间隔点ID，就会退出
+for page in itertools.count(1):
+    url = 'http://example.webscraping.com/view/-%d' % page
+    html = download(url)
+    if html is None:
+        break
+    else:
+        # success - can scrape the result
+        pass
+
+# 下面是改进版代码,连续下载出错max_errors次才会停止遍历
+max_errors = 5
+num_errors = 0
+for page in itertools.count(1):
+    url = 'http://example.webscraping.com/view/-%d' % page
+    html = download(url)
+    if html is None:
+        num_errors += 1
+        if num_errors >= max_errors:
+            break
+    else:
+        # success - can scrape the result
+        num_errors = 0
