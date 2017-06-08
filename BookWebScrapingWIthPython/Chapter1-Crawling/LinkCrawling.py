31d0d86dac87ffc3cc8be434f4004efb1e0324da
zsdostar <zsdzzydos@gmail.com>
2017-06-08 14:31:18 +0800

    现在，它能够为爬取过的页面添加标记，能够如期的停止，所以这是一个可用的爬虫

--- BookWebScrapingWIthPython/Chapter1-Crawling/LinkCrawling.py
+++ BookWebScrapingWIthPython/Chapter1-Crawling/LinkCrawling.py
@@ -4,17 +4,22 @@
 import re
 import urlparse
 from BasicURLCrawlingCode import download
+"""这是一个链接爬虫，能够爬取所有从URL追踪到的链接"""
 
 
 def link_crawler(seed_url, link_regex):
     crawl_queue = [seed_url]
+    has_seen = set(crawl_queue)
     while crawl_queue:
         url = crawl_queue.pop()
         html = download(url)
         for link in get_links(html):
             if re.match(link_regex, link):
-                link = urlparse.urljoin(seed_url, link)
-                crawl_queue.append(link)
+                link = urlparse.urljoin(seed_url, link)  # 相对路径变成绝对路径
+                if link not in has_seen:
+                    has_seen.add(link)
+                    crawl_queue.append(link)
+
 
 
 def get_links(html):
