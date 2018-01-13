4d48bbd2cf3655a40720c218686aac1bb3582c03
zsdostar <zsdzzydos@gmail.com>
2018-01-13 23:58:53 +0800

    面试复习

--- Course-SeniorPy/Chapter8装饰器/__init__.py
+++ Course-SeniorPy/Chapter8装饰器/__init__.py
@@ -10,3 +10,16 @@
 --------------------------------------------------------------------------
 """
 
+
+class User(object):
+    def __init__(self, first_name, last_name, **other_info):
+        self.first_name = first_name
+        self.last_name = last_name
+        for k,v in other_info.items():
+            setattr(self, k, v)
+            # print k,v
+
+
+
+u = User('Z', 'sd', age=21)
+# print u.age
